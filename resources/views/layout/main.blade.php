2d6c133c944a503bd81b4023fe7ccbc274bcffa3
Robby Novianto <robbyhahaha@gmail.com>
2021-03-09 15:37:45 +0700

    merubah gambar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -43,17 +43,17 @@
       </nav>
 {{-- Akhir Navbar --}}
 
+
 {{-- Jumbotron --}}
-<div class="jumbotron jumbotron-fluid">
+<div class="jumbotron">
     <div class="container">
-      <h1 class="display-4"></h1>
-      <p class="lead"></p>
+    <h1 class="display-4">BARVE</h1>
+    <p>Aplikasi</p>
     </div>
   </div>
 {{-- Akhir Jumbotron --}}
     @yield('container')
     <!-- Optional JavaScript; choose one of the two! -->
-
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
