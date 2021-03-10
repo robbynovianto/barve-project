7673c86cf5348e794aeab6ae594eeba58776aefa
Robby Novianto <robbyhahaha@gmail.com>
2021-03-10 10:34:35 +0700

    merapikan css dan img kedalam folder

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,11 +9,12 @@
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Viga&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
         </style>
 
     {{-- My CSS --}}
     <script src="js/script.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="asset/css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
